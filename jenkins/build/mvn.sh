de63dec59f1cb94a062a977772e326e4e55d4f0e
Eduardo <eloya@minsait.com>
2021-09-18 16:12:01 +0200

    new changes

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,5 +4,6 @@ echo "****************"
 echo "* Building jar!*"
 echo "****************"
 
+echo $whoami
 PROJ=/var/lib/jenkins/workspace/pipeline-docker-maven
 docker run --rm -v /root/.m2:/root/.m2 -v $PROJ/java-app:/app -w /app maven:3-alpine "$@"
